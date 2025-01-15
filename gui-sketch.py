570276080c68f3404a272978e20c20bb994c8da5
Ram Sakalle <129431348+ramsakalle45@users.noreply.github.com>
2025-01-15 18:03:57 +0530

    Update gui-sketch.py

--- gui-sketch.py
+++ gui-sketch.py
@@ -7,11 +7,11 @@ from typing import Tuple, Optional, Dict
 import logging
 from datetime import datetime
 
-from pyvirtualdisplay import Display
+# from pyvirtualdisplay import Display
 
-# Start a virtual display
-display = Display(visible=0, size=(800, 600))
-display.start()
+# # Start a virtual display
+# display = Display(visible=0, size=(800, 600))
+# display.start()
 
 
 # ----------------------------------------------------
